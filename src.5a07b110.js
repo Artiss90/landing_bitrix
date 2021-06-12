c83a426e5eedf3f0e2a4e9923e50c5bae2f89a36
Artem <64410376+Artiss90@users.noreply.github.com>
2021-06-12 12:56:10 +0300

    Updates

--- src.5a07b110.js
+++ src.5a07b110.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Landing_company/src.5a07b110.js.map
+//# sourceMappingURL=/landing_bitrix/src.5a07b110.js.map
